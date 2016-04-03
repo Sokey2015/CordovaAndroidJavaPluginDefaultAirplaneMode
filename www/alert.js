4d8e58479946126ed4acf62c22a998ac1365a4b6
theonlycaosun <michaelcaosun@Michaels-MacBook-Pro-2.local>
2016-04-02 20:28:21 -0400

    Added files

---
+++ www/alert.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+module.exports = {
+  alert: function(title, message, buttonLabel, successCallback) {
+    cordova.exec(successCallback,
+                 null, // No failure callback
+                 "Alert",
+                 "alert",
+                 [title, message, buttonLabel]);
+  }
+};
